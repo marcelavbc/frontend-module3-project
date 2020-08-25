da6b050e1d242fb403db939c6b0995014975abd9
Marcela Vilas Boas <marcela.vilasboas@gmail.com>
2020-08-25 08:10:20 +0200

    fix search

--- src/components/recipes/Recipes.js
+++ src/components/recipes/Recipes.js
@@ -34,37 +34,31 @@ export default class Recipes extends Component {
 
 
         return (
-            <div className="col-12 recipe-card card">
-                <div className="panel-group">
-
-                    <div className="panel-title">
-                        <h5 className="card-title text-center">{this.props.title}</h5>
-
-                        <img className="recipe-image" src={this.props.src} alt={this.props.title} />
-                        <div className="card-icons">
-                            <div className="wrapper">
-                                <i className="icon far fa-clock"></i>
-                                <span className="badge minuts">{this.props.minutes}</span>
-                            </div>
-                            <div className="wrapper">
-                                <i className="icon fas fa-utensils"></i>
-                                <span className="badge">{this.props.serving}</span>
-                            </div>
-                            <div className="wrapper">
-                                <i className={this.state.open ? "icon fas fa-question-circle" : "icon far fa-question-circle"} onClick={this.open}></i>
-                                <span className="badge">{this.props.missed}</span>
-                            </div>
-                            <Link to={`/recipe/${this.props.id}`}><i className="icon fas fa-info" onClick={this.handleIconClick}></i></Link>
-                            <i className={this.state.saved ? "icon fas fa-bookmark" : "icon far fa-bookmark"}></i>
-                        </div>
+            <div className="recipe-card card col-md-3 mx-3">
+                 <h5 className="card-title text-center">{this.props.title}</h5>
+                    <img className="recipe-image" src={this.props.src} alt={this.props.title} />
+                <div className="card-icons">
+                    <div className="wrapper">
+                        <i className="icon far fa-clock"></i>
+                        <span className="badge minuts">{this.props.minutes}</span>
                     </div>
-
-                    <div className={this.state.open ? "panel-collapse" : "panel-collapse panel-close"}>
-                        <ul>
-                            {this.props.missing.map((ele, i) => <li className="miss-ingredients-list" key={i}>{ele.name}</li>)}
-                        </ul>
+                    <div className="wrapper">
+                        <i className="icon fas fa-utensils"></i>
+                        <span className="badge">{this.props.serving}</span>
                     </div>
+                    <div className="wrapper">
+                        <i className={this.state.open ? "icon fas fa-question-circle" : "icon far fa-question-circle"} onClick={this.open}></i>
+                        <span className="badge">{this.props.missed}</span>
+                    </div>
+                    <Link to={`/recipe/${this.props.id}`}><i className="icon fas fa-info" onClick={this.handleIconClick}></i></Link>
+                    <i className={this.state.saved ? "icon fas fa-bookmark" : "icon far fa-bookmark"}></i>
                 </div>
+
+                <div className={this.state.open ? "panel-collapse" : "panel-collapse panel-close"}>
+                    <ul>
+                        {this.props.missing.map((ele, i) => <li className="miss-ingredients-list" key={i}>{ele.name}</li>)}
+                    </ul>
+                </div> 
             </div>
         )
     }
